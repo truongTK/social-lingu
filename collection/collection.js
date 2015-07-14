d1a73d3755fcaa9c55aac677af596de7c1e1444f
Tran Kim Truong <truongprobk1992@gmail.com>
2015-07-14 10:55:15 +0700

    Declare All Collection

--- collection/collection.js
+++ collection/collection.js
@@ -1,2 +1,10 @@
-Sentences = new Mongo.Collection("sentences");
-Translate = new Mongo.Collection("translate");
+/**
+ * @Discription: Declare the Model
+ * @Author: TruongTK
+ */
+
+UserInfo = new Mongo.Collection('userinfo');
+Languages = new Mongo.Collection('languages');
+Request = new Mongo.Collection('request');
+Reply = new Mongo.Collection('reply');
+Rating = new Mongo.Collection('rating');
