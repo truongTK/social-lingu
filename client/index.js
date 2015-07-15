5bb3ad9fcb4c2b0ef0c64248b8fc9bfb71043f09
toanpp <toanpp@twin.vn>
2015-07-15 14:54:53 +0700

    fix conflict

--- client/index.js
+++ client/index.js
@@ -1,55 +1,7 @@
 /**
- * @Discription: Base javascript of index
- * @Author: TruongTK
- */
-<<<<<<< HEAD
- if (Meteor.isClient) {
-   // To store the slideout instance.
-   var slideout;
-
-   // Router configuration
-   Router.configure({
-     layoutTemplate: 'MasterLayout'
-   });
-
-   // Auto-close the menu on route stop (when navigating to a new route)
-   Router.onStop(function () {
-     if (slideout) {
-       slideout.close();
-     }
-   });
-
-   // Define some routes
-   Router.route('/', { name: 'homeTemplate' });
-   Router.route('/myrequest', { name: 'showRequest' });
-   Router.route('/profile', { name: 'loginTemplate' });
-   Router.route('/signup', { name: 'signupTemplate' });
-
-   // Setup code for Slideout menu in MasterLayout
-   Template.MasterLayout.onRendered(function () {
-     var template = this;
-     slideout = new Slideout({
-       'panel': template.$('#content').get(0),
-       'menu': template.$('#slideout-menu').get(0),
-       'padding': 256,
-       'tolerance': 70
-     });
-   });
-
-   Template.MasterLayout.events({
-     'click #btnMenu': function() {
-       // increment the counter when button is clicked
-       slideout.toggle();
-     }
-   });
-   // Other template code
-
-
- }
-
-=======
->>>>>>> 6a284ecc1658301506585cc6a2a9f54680a17ec4
-
+* @Discription: Base javascript of index
+* @Author: TruongTK
+*/
 Meteor.subscribe('userinfo');
 Meteor.subscribe('languages');
 Meteor.subscribe('request');
@@ -81,22 +33,26 @@ Router.route('/myrequest', {
 Router.route('/otherRequest', {
   name: 'showOtherRequest'
 });
+Router.route('/profile', {
+  name: 'loginTemplate' });
+Router.route('/signup', {
+  name: 'signupTemplate' });
 
-// Setup code for Slideout menu in MasterLayout
-Template.MasterLayout.onRendered(function() {
-  var template = this;
-  slideout = new Slideout({
-    'panel': template.$('#content').get(0),
-    'menu': template.$('#slideout-menu').get(0),
-    'padding': 256,
-    'tolerance': 70
-  });
-});
+    // Setup code for Slideout menu in MasterLayout
+    Template.MasterLayout.onRendered(function() {
+      var template = this;
+      slideout = new Slideout({
+        'panel': template.$('#content').get(0),
+        'menu': template.$('#slideout-menu').get(0),
+        'padding': 256,
+        'tolerance': 70
+      });
+    });
 
-Template.MasterLayout.events({
-  'click #btnMenu': function() {
-    // increment the counter when button is clicked
-    slideout.toggle();
-  }
-});
-// Other template code
+    Template.MasterLayout.events({
+      'click #btnMenu': function() {
+        // increment the counter when button is clicked
+        slideout.toggle();
+      }
+    });
+    // Other template code
