894d82ad67662c4643239e6490b9eed296a6445e
toanpp <toanpp@twin.vn>
2015-07-16 14:30:41 +0700

    to get new code

--- client/user/profile.js
+++ client/user/profile.js
@@ -4,5 +4,19 @@ Template.profileTemplate.helpers({
       return true;
     }
     return false;
+  },
+});
+Template.profileTemplate.helpers({
+  listStudyLanguage: function() {
+    $('#studyLanguage').selectpicker('mobile');
+    return Languages.find({});
+  },
+  listFluentLanguage: function() {
+    $('#fluentLanguage').selectpicker('mobile');
+    return Languages.find({});
   }
+});
+Template.profileTemplate.onRendered(function() {
+  $('#studyLanguage').selectpicker('render');
+  $('#fluentLanguage').selectpicker('render');
 });
